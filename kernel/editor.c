c2fb3fa3a6e91d4bbba729d36d84dafa75cca80a
prabhutvasingh <prabhutvasingh@github.com>
2026-09-05 13:08:38 +0530

    editor: banner only for nano-official, nano stable quiet Edit

--- kernel/editor.c
+++ kernel/editor.c
@@ -11,14 +11,16 @@ int elf_load(void* data, void (**entry)(void));
 static int force_official=0;
 void editor_set_official(int v){ force_official=v; }
 void editor_open(const char* path){
-    const char *msg1="  GNU nano 9.2 official (savannah.gnu.org) 660K static musl -> embedded LBA 4121\n";
-    const char *msg2="  Title: Edit  (nano renamed to editor)  File: ";
-    for(const char *a=msg1;*a;a++) outb(0x3F8,*a);
-    for(const char *a=msg2;*a;a++) outb(0x3F8,*a);
-    if(path) for(const char *a=path;*a;a++) outb(0x3F8,*a);
-    outb(0x3F8,'\n');
-    const char *info="  Official nano ELF: /tmp/nano-9.2/src/nano 660K static stripped -> StrixOS disk LBA 4121\n";
-    for(const char *a=info;*a;a++) outb(0x3F8,*a);
+    if(force_official){
+        const char *msg1="  GNU nano 9.2 official (savannah.gnu.org) 660K static musl -> embedded LBA 4121\n";
+        const char *msg2="  Title: Edit  (nano renamed to editor)  File: ";
+        for(const char *a=msg1;*a;a++) outb(0x3F8,*a);
+        for(const char *a=msg2;*a;a++) outb(0x3F8,*a);
+        if(path) for(const char *a=path;*a;a++) outb(0x3F8,*a);
+        outb(0x3F8,'\n');
+        const char *info="  Official nano ELF: /tmp/nano-9.2/src/nano 660K static stripped -> StrixOS disk LBA 4121\n";
+        for(const char *a=info;*a;a++) outb(0x3F8,*a);
+    }
     if(force_official){
         void *nano_data=elf_load_nano_lba(4121);
         if(nano_data){
